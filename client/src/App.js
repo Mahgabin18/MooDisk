cf0b4de1ff811f0b4cfa312f35548957286894f2
flabica <flabicamaharjan@gmail.com>
2022-03-26 18:12:49 -0400

    uploaded the basic skeleton for the wordsearch page, created checkboxes for the wordsearch

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,82 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
+//import logo from './logo.svg';
+import React from "react";
+// import { useState } from 'react'
+// import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from "./components/Login";
+import WordSearch from "./components/WordSearch"
 
 
-const App = () => {
-	return (
-		<div>
-		<Router>
-            <Routes>
-            <Route exact path="/" element={<Register/>} />
-            <Route exact path="/login" element={<Login/>} />
-            <Route exact path="/register" element={<Register/>} />
-            <Route exact path="/dashboard" element={<Dashboard/>} />
-            </Routes>
-      </Router>
-		</div>
-	)
+export default function App() {
+return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/wordsearch" element={<WordSearch/>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+
+
+// function App() {
+//   const [name, setName] = useState('')
+//   const [email, setEmail] = useState('')
+// 	const [password, setPassword] = useState('')
+  
+//   async function registerUser(event){
+//     event.preventDefault()
+//     const response = await fetch('http://localhost:1337/api/register',{
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         name,
+//         email,
+//         password,
+//       }),
+//     })
+//     const data = await response.json()
+//     console.log(data)
+//   }
+//   return (
+//     <div>
+//       <h1>Register</h1>   
+//       <form onSubmit={registerUser}>
+//         <input
+//         value={name}
+//         onChange={(e) => setName(e.target.value)}
+//         type="text"
+//         placeholder="Name"
+//          /><br/>
+//         <input
+//         value={email}
+//         onChange={(e) => setEmail(e.target.value)}
+//         type="email"
+//         placeholder="Email"
+//          /><br/>
+//         <input
+//         value={password}
+//         onChange={(e) => setPassword(e.target.value)}
+//         type="password"
+//         placeholder="Password"
+//          /><br/>
+//          <input type="submit" value="Register" />
+//       </form>
+        
+//         <a href="./wordsearch.js" target="_blank">
+//         <Button> Link Button </Button>
+// </a>
+      
+//      </div>
+//   );
+// }
+
+// export default App;
